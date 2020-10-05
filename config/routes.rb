9aa99c7ef31ad6b3252cb7d31a6c443035a21c53
andresochoa91 <andres_ochoa91@hotmail.com>
2020-10-05 00:26:27 -0700

    Some updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do 
+    resources :comments
+    # get    '/comments',          to: 'comments#index', as: 'comments'
+    # post   '/comments',          to: 'comments#create'
+    # get    '/comments/new',      to: 'comments#new',   as: 'new_comment'
+    # get    '/comments/:id',      to: 'comments#show',  as: 'post_comment'
+    # get    '/comments/:id/edit', to: 'comments#edit',  as: 'edit_comment'
+    # patch  '/comments/:id',      to: 'comments#update'
+    # delete '/comments/:id',      to: 'comments#destroy'
+  end
 
   # get '/pages', to: 'pages#index'
   # post '/pages', to: 'pages#create'
@@ -8,6 +17,7 @@ Rails.application.routes.draw do
   # get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
   # patch '/pages/:id', to: 'pages#update'
   # delete '/pages/:id', to: 'pages#destroy'
-  
+
+
   resources :pages #this is basically the same of above, but shorter 
 end
